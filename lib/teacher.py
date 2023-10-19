50d118c88d5068b2741a8dcbda5f086c73afb20d
preciousnjukia <preciousnjukia@gmail.com>
2023-10-19 14:10:53 +0300

    Assignment done

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,27 @@ from user import User
 
 import random
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 class Teacher(User):
+        def __init__(self, first_name, last_name, knowledge):
+                super().__init__(first_name, last_name)
+                self.knowledge = knowledge
+def teach(self):
+        if self.knowledge:
+            random_index = random.randint(0, len(self.knowledge) - 1)
+            return self.knowledge[random_index]
+        else:
+            return "I don't have any knowledge to share."
+        
 
-    def teach(self):
-        pass
+
+my_teacher = Teacher("My", "Teacher", knowledge)
